f0f069be5190efe26f2ba8b7536508d4a7da0b91
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-09-10 08:53:38 +0700

    modify with callback

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ import { converterDate } from "./lib/converter.js";
 import { converterDateManual } from "./lib/converterManual.js";
 import { interfaceInput } from "./lib/interfaceInput.js";
 
-async function main() {
-  try {
-    console.log(" ----- Selamat Datang di Converter Date ----- \n");
-    const input = await interfaceInput(" Masukkan Tanggal (dd-mm-yy): ");
-    const convert = await converterDate(input);
-    const convertManual = await converterDateManual(input);
-    console.log(`\n Hasil Konversi dengan moment: ${convert}`);
-    console.log(`\n Hasil Konversi tanpa moment: ${convertManual}`);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-main();
+console.log(" ----- Selamat Datang di Converter Date ----- \n");
+interfaceInput(" Masukkan Tanggal (dengan moment): ", (input1) => {
+  converterDate(input1);
+  interfaceInput(" Masukkan Tanggal (Tanpa Moment): ", (input2) => {
+    converterDateManual(input2);
+  });
+});
